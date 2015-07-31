690820e843db3218f0cb38ed285f928095d46ab8
Alban M <github@mussi.fr>
2015-07-31 15:33:26 +0200

    [install-AWS-cli] making some chang ein env_aws.sh

--- env_aws.sh
+++ env_aws.sh
@@ -1,9 +1,7 @@
-# 
-# AWS variables
-#
+#---------------------------- 
+# AWS cli install 
+#---------------------------- 
 export INSTALL_PATH="${HOME}/bin/AWS"
-
-export JAVA_HOME=
 export EC2_HOME=${INSTALL_PATH}/ec2
 export AWS_IAM_HOME=${INSTALL_PATH}/iam
 export AWS_RDS_HOME=${INSTALL_PATH}/rds
@@ -17,28 +15,32 @@ export AWS_SNS_HOME=${INSTALL_PATH}/sns
 export AWS_ROUTE53_HOME=${INSTALL_PATH}/route53
 export AWS_CLOUDFRONT_HOME=${INSTALL_PATH}/cloudfront
 
+# Set your java installation path
+export JAVA_HOME=
 
-export AWS_ACCESS_KEY=<your acess key here>
-export AWS_SECRET_KEY=<your secret key here>
-
+# Export PATH
 for BIN_DIR in $(find ${INSTALL_PATH} -type d -name bin)
 do
   export PATH=${BIN_DIR}:$PATH
 done
-
-PATH=${INSTALL_PATH}/elasticbeanstalk/eb/linux/python2.7:$PATH
-PATH=${INSTALL_PATH}/elasticmapreduce:$PATH
+#-----------------------------
+# Authentification 
+#-----------------------------
 
 export EC2_PRIVATE_KEY=$(echo $HOME/.aws-default/pk-*.pem)
 export EC2_CERT=$(echo $HOME/.aws-default/cert-*.pem)
-export AWS_CREDENTIAL_FILE=$HOME/.aws-default/aws-credential-file.txt
-export ELASTIC_MAPREDUCE_CREDENTIALS=$HOME/.aws-default/aws-credentials.json
 
 # Some tools use AWS_ACCESS_KEY, others use AWS_ACCESS_KEY_ID
 export AWS_ACCESS_KEY=< your access key ID >
 export AWS_SECRET_KEY=< your secret access key >
+#
 export AWS_ACCESS_KEY_ID=< your access key ID >
 export AWS_SECRET_SECRET_KEY=< your secret access key >
+export AWS_CREDENTIAL_FILE=$HOME/.aws/aws-credential-file.txt
 
-# Change the default region if desired
-#export EC2_REGION=eu-west-1
+#------------------------------
+# Other products
+#------------------------------
+export ELASTIC_MAPREDUCE_CREDENTIALS=$HOME/.aws-default/aws-credentials.json
+PATH=${INSTALL_PATH}/elasticbeanstalk/eb/linux/python2.7:$PATH
+PATH=${INSTALL_PATH}/elasticmapreduce:$PATH
